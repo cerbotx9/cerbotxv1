a756728823f3146981df23ac9c23215c351103fa
Enrique Perezc <cerbotx9@gmail.com>
2019-08-09 03:02:26 -0500

    Commit Principal Basico

---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CERBOTX Y</title>
+
+    <!--CSS Boostrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/principales.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+
+
+
+</head>
+<body>
+    
+<style>
+
+.left, .right {
+  float: left;
+  width: 20%; /* The width is 20%, by default */
+}
+
+.main {
+  float: left;
+  width: 60%; /* The width is 60%, by default */
+}
+
+/* Use a media query to add a breakpoint at 800px: */
+@media screen and (max-width: 800px) {
+  .left, .main, .right {
+    width: 100%; /* The width is 100%, when the viewport is 800px or smaller */
+  }
+}
+
+
+#particles-js{
+
+position: absolute;
+left: 0px;
+top: 0px;
+z-index: -1;
+
+left:0;
+width:100%;
+height: 90%;
+
+
+}
+#centerObject{
+
+margin-inline: 20%;
+background-position: center;
+
+}
+
+
+hr{
+
+color: red;
+
+
+}
+.img:hover {
+  
+-webkit-transform: rotateY(s180deg);
+-webkit-transform-style: preserve-3d;
+transform: rotateY(180deg);
+transform-style: preserve-3d;
+
+}
+footer{
+
+background-color: black;
+
+}
+.footpage{
+
+margin-inline: 10%;
+
+
+}
+
+</style>
+
+<!-- Particulas -->
+ <div id="particles-js">
+    
+    </div>
+    <script src="particles.js"></script>
+    <script src="particulas.js"></script>
+    
+   <div >
+
+
+        <!-- Barra de Navegacion -->
+        
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">CERBOTX Y</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Acerca de CERBOTX</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contacto CERBOTX</a>
+            </li>
+          </ul>
+          <span class="navbar-text">
+      Con la mira en alto y con fuerza se sigue adelante!    </span>
+        </div>
+      </nav>
+      <hr color="red">
+
+      <!-- Fin de la barra de navegacion-->
+
+      <div id="centerObject">
+<img src="img/publicidad/ROG.png" class="img" alt="Responsive image" width="45%" height="15%">
+<img src="img/publicidad/ROG.png" class="img" alt="Responsive image" width="45%" height="15%">
+
+<hr color="red">
+
+<h1 class="display-5" align="center"><b>CERBOTX Y</b></h1>
+<h1 ALING="LEFT" >CASA DE DESARROLLO WEB APP 4.0</h1>
+<hr >
+   
+ 
+    <h3> <B>MISI&Oacute;N </B></h3>
+    <font face="arial narrow">
+    <p> Desarrollar software integrando los mas alto 
+      estandares de calidad y utilizando las nuevas 
+      tecnologias de desarrollo e inovando la tecnologia 
+      de la mano de los requisitos de la Industria 4.0.</p>
+    </font>
+    <h3> <B>VISI&Oacute;N </B></h3>
+    <font face="arial narrow">
+    <p > 
+      Cubrir las mas exigentes necesidades y 
+      requerimentos de la gran Industria, adaptandonos 
+      a las nuevas tecnologias del desarrollo de software.
+      Asi como consolidar la seguridad de nuestros proyectos 
+      para colocarnos como la mejor casa de desarrollo de
+       software en el mundo.
+      </p>
+</font>
+  
+</div>
+
+
+
+    <!-- jQuery -->
+    <script src="js/jquery-2.1.1.js"></script>
+    <!--  plugins -->
+    <script src="js/bootstrap.min.js"></script>
+
+   
+    <!-- google analytics  -->
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-76796224-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+
+
+
+<!--Scripts Boostrap -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<footer> 
+  <div class="footpage">
+  <font  size="4em" color="white">
+
+ <ol> Contacto:
+  <li> Email:</li>
+  <li> Telefono:</li>
+  <li> Email Alternativo:</li>
+
+ </ol>
+
+
+</font>
+      </div>
+</footer>
+
+
+</body>
+
+</html>
